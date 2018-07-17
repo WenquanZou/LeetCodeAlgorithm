95cffba02d23110396a152819efbb9de9146f8e7
WenquanZou <wz1816@ic.ac.uk>
2018-07-17 22:46:44 +0100

    Change power method to recursive

--- Solution/Power.kt
+++ Solution/Power.kt
@@ -1,14 +1,25 @@
 class Power {
 
-  fun pow(x: Double, n: Int): Double {
-    var a = x
-    var number = n
-    if (number == 0)
+  private fun fastPow(x: Double, n: Long): Double {
+    if (n == 0.toLong()) {
       return 1.0
-    if (number < 0) {
-      number = -number
-      a = 1 / a
     }
-    return if (number % 2 == 0) pow(a * a, number / 2) else a * pow(a * a, number / 2)
+    val half = fastPow(x, n / 2)
+    return if (n % 2 == 0.toLong()) {
+      half * half
+    } else {
+      half * half * x
+    }
+  }
+
+  fun myPow(x: Double, n: Int): Double {
+    var x = x
+    var N = n.toLong()
+    if (N < 0) {
+      x = 1 / x
+      N = -N
+    }
+
+    return fastPow(x, N)
   }
 }
