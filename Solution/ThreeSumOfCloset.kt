82db585e9eab3213c7e6066630e2a6e2c47a1adf
WenquanZou <wz1816@ic.ac.uk>
2018-07-21 23:46:43 +0100

    07-21 Problem three sum of closet

---
+++ Solution/ThreeSumOfCloset.kt
@@ -0,0 +1,37 @@
+
+import java.util.Arrays
+
+class ThreeSumOfCloset {
+  fun threeSumClosest(nums: IntArray, target: Int): Int {
+
+    if (nums.isEmpty()) {
+      return 0
+    }
+    nums.sort()
+
+    var i = 0
+    var diff = Integer.MAX_VALUE
+    var res = 0
+    while (i < nums.size - 2) {
+      var l = i + 1
+      var r = nums.size - 1
+      while (l < r) {
+        val tmp = nums[i] + nums[l] + nums[r]
+        val absDiff = Math.abs(tmp-target)
+        if (absDiff < diff) {
+          diff = absDiff
+          res = tmp
+        }
+
+        when {
+          tmp > target -> r -= 1
+          tmp < target -> l += 1
+          else -> return tmp
+        }
+      }
+      i++
+    }
+    return res
+  }
+
+}
