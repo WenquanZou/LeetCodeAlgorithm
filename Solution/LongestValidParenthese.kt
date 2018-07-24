e8ca28622ca9e5c6f715a1ba1f0e0da3a8b186fb
WenquanZou <wz1816@ic.ac.uk>
2018-07-24 07:10:02 +0100

    07-24 Problem Longest Valid Parentheses

---
+++ Solution/LongestValidParenthese.kt
@@ -0,0 +1,22 @@
+import java.util.Stack
+
+class LongestValidParenthese {
+  fun longestValidParentheses(s: String): Int {
+    var maxans = 0
+    val stack = Stack<Int>()
+    stack.push(-1)
+    for (i in 0 until s.length) {
+      if (s[i] == '(') {
+        stack.push(i)
+      } else {
+        stack.pop()
+        if (stack.empty()) {
+          stack.push(i)
+        } else {
+          maxans = Math.max(maxans, i - stack.peek())
+        }
+      }
+    }
+    return maxans
+  }
+}
