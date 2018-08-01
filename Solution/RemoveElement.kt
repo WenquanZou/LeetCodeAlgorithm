c8df9fc1897de532bc4ad80a53637dfa723a4bdc
WenquanZou <wz1816@ic.ac.uk>
2018-08-01 17:41:37 +0100

    07-31 Problem remove element

---
+++ Solution/RemoveElement.kt
@@ -0,0 +1,12 @@
+class RemoveElement {
+  fun removeElement(nums: IntArray, `val`: Int): Int {
+    var i = 0
+    for (j in nums.indices) {
+      if (nums[j] != `val`) {
+        nums[i] = nums[j]
+        i++
+      }
+    }
+    return i
+  }
+}
