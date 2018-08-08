99f31eae2bcbbd68f60030ba5d14be42e0ee0d38
WenquanZou <wz1816@ic.ac.uk>
2018-08-08 15:13:22 +0100

    08-08 Problem Zig Zag Conversion

---
+++ Solution/ZigZagConversion.kt
@@ -0,0 +1,21 @@
+class ZigZagConversion {
+  fun convert(s: String, numRows: Int): String {
+
+    if (numRows == 1) return s
+
+    val ret = StringBuilder()
+    val n = s.length
+    val cycleLen = 2 * numRows - 2
+
+    for (i in 0..numRows - 1) {
+      var j = 0
+      while (j + i < n) {
+        ret.append(s[j + i])
+        if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+          ret.append(s[j + cycleLen - i])
+        j += cycleLen
+      }
+    }
+    return ret.toString()
+  }
+}
