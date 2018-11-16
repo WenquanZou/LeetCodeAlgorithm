a0c2f64d37e791d266c19aa767d64fbcb2f3be8b
Wenquan Zou <wz1816@ic.ac.uk>
2018-11-16 19:46:45 +0000

    11-16 Solution to kth largest_element

---
+++ Solution/kth_largest_element.py
@@ -0,0 +1,9 @@
+class Solution:
+    @staticmethod
+    def findKthLargest(nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return sorted(nums, reverse=True)[k - 1]
