1b39c65d3a6cda778cdecdbfc9094483defb8c22
WenquanZou <wz1816@ic.ac.uk>
2018-11-16 07:14:05 +0000

    11-16 Solution to merge intervals

---
+++ Solution/merge_intervals.py
@@ -0,0 +1,17 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+
+class Solution(object):
+    @staticmethod
+    def merge(intervals):
+        out = []
+        for i in sorted(intervals, key=lambda i: i.start):
+            if out and i.start <= out[-1].end:
+                out[-1].end = max(out[-1].end, i.end)
+            else:
+                out.append(i)
+        return out
